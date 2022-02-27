5b6be789bb1b7e0a5802449f569b4f1a3857b5b6
ANDRES\andre <andresfelipepatino5@gmail.com>
2022-02-27 16:53:44 -0500

    Responsive

--- wp-content/plugins/products-draketech/public/shortcode/assets/shortcode.js
+++ wp-content/plugins/products-draketech/public/shortcode/assets/shortcode.js
@@ -7,7 +7,31 @@ jQuery(document).ready(function ($) {
         autoplay: true,
         infinite: true,
         autoplaySpeed: 5000,
-        dots: true
+        dots: true,
+        responsive: [
+            {
+              breakpoint: 1025,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+              }
+            },
+            {
+              breakpoint: 769,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                dots: false
+              }
+            },
+            {
+              breakpoint: 575,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
     });
 
 });
