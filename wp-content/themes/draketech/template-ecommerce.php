78dd8da77c3323066246e857f525e0d53e2b56c7
ANDRES\andre <andresfelipepatino5@gmail.com>
2022-02-27 10:53:03 -0500

    CTA icons

--- wp-content/themes/draketech/template-ecommerce.php
+++ wp-content/themes/draketech/template-ecommerce.php
@@ -19,7 +19,7 @@ get_header(); ?>
                 <a class="button" href="#">Shop Now</a>
             </div>
             <div class="col">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/prod-banner1.jpg" alt="">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/prod-banner1.jpg" alt="Product 1">
             </div>
         </div>
     </div>
@@ -31,11 +31,53 @@ get_header(); ?>
                 <a class="button" href="#">Shop Now</a>
             </div>
             <div class="col">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/prod-banner2.jpg" alt="">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/prod-banner2.jpg" alt="product 2">
             </div>
         </div>
     </div>    
 </section>
+
+<section class="icons">
+    <div class="content-area">
+        <div class="box">
+            <div class="icon">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/home-ico.svg" alt="Home shipping">
+            </div>
+            <div class="text">
+                <p class="title">Home Shipping</p>
+                <p class="subtitle">Free for all oder</p>
+            </div>
+        </div>
+        <div class="box">
+            <div class="icon">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/send-paper-ico.svg" alt="100% refound">
+            </div>
+            <div class="text">
+                <p class="title">100% Refund</p>
+                <p class="subtitle">Cash Back</p>
+            </div>
+        </div>
+        <div class="box">
+            <div class="icon">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/shield-ico.svg" alt="Clients support">
+            </div>
+            <div class="text">
+                <p class="title">Clients′ Support</p>
+                <p class="subtitle">Fast Service </p>
+            </div>
+        </div>
+        <div class="box">
+            <div class="icon">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/rocket.svg" alt="Fast Delivery">
+            </div>
+            <div class="text">
+                <p class="title">Fast Delivery</p>
+                <p class="subtitle">Best Service</p>
+            </div>
+        </div>
+    </div>
+</section>
+
 
 <div id="primary" class="content-area">
    
