0e7dd3e8409042b8b062c8b602c48db0d378ef15
ANDRES\andre <andresfelipepatino5@gmail.com>
2022-02-27 11:53:14 -0500

    Register postype products

---
+++ wp-content/plugins/products-draketech/admin/postype_products.php
@@ -0,0 +1,41 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {	exit; }
+
+function products_post_type() {
+	$labels = array(
+        'name'                  => _x( 'Products', 'Post type general name', 'DKT' ),
+        'singular_name'         => _x( 'Product', 'Post type singular name', 'DKT' ),
+        'menu_name'             => _x( 'Products', 'Admin Menu text', 'DKT' ),
+        'add_new'               => __( 'Add product', 'DKT' ),
+        'add_new_item'          => __( 'Add new product', 'DKT' ),
+        'new_item'              => __( 'New product', 'DKT' ),
+        'edit_item'             => __( 'Edit product', 'DKT' ),
+        'view_item'             => __( 'View product', 'DKT' ),
+        'all_items'             => __( 'All products', 'DKT' ),
+        'search_items'          => __( 'Search product', 'DKT' ),
+        'not_found'             => __( 'Product not found', 'DKT' ),
+        'not_found_in_trash'    => __( 'Product not found', 'DKT' ),
+        'featured_image'        => _x( 'Thumbnail', '', 'DKT' ),
+    );    
+	$args = array(
+        'labels'             => $labels,
+		'public'             => true,
+        'show_ui'            => true,	
+        'has_archive'        => false,
+        'rewrite'            => array( 'slug' => 'products'),
+		'capability_type'    => 'post',
+		'menu_icon'          => 'dashicons-cart',
+		'menu_position'      => 5,
+		'supports'           => array('title','editor','thumbnail')
+	);
+	register_post_type( 'products', $args );	
+}
+add_action( 'init', 'products_post_type' );
+
+
+/* Flush Rewrite - regenera .htacess*/
+function products_rewrite_flush(){
+    products_post_type();
+    flush_rewrite_rules();
+}  
---
+++ wp-content/plugins/products-draketech/plugin-products-draketech.php
@@ -0,0 +1,25 @@
+<?php 
+
+/**
+ * Plugin Name:       Products DrakeTech
+ * Plugin URI:        https://github.com/andresfpatino
+ * Description:       Plugin creación de productos - Prueba desarrollo DrakeTech
+ * Version:           1.0
+ * Author:            Andrés Patiño
+ * Author URI:        https://github.com/andresfpatino
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://github.com/andresfpatino
+ * Text Domain:       DKT
+ */
+
+
+define( 'DKT_PATH', plugin_dir_path( __FILE__ ) );
+
+
+// Postype 
+require_once DKT_PATH."/admin/postype_products.php";
+
+// Rewrite Flush
+register_activation_hook(__FILE__, 'products_rewrite_flush');
+register_deactivation_hook( __FILE__, 'products_rewrite_flush' );
